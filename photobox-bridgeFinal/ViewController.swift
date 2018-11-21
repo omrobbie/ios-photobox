96ebefe0741597ec41a8599652a6a66ed0c0422c
Hubert Wang <hubert.tatra@gmail.com>
2018-11-21 10:49:08 +0700

    Initial commit

---
+++ photobox-bridgeFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photobox-bridgeFinal
+//
+//  Created by Hubert Wang on 21/11/18.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
